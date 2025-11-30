7295a8b85d4fd13aefc08d8c0f205f1c19c270c9
Pramudithalakshan <pramudithalakshan00@gmail.com>
2025-11-30 09:11:49 +0530

    Create customer and orders manage part

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// let customerArray = [];
+// localStorage.setItem("customers", JSON.stringify(customerArray));
 
 fetch("nav.html")
     .then(res => res.text())
@@ -15,14 +17,14 @@ function login() {
     const adminDetails = JSON.parse(localStorage.getItem("loginDetails"));
 
     if (!adminDetails) {
-        console.log("No login details found in localStorage!");
+        Swal.fire("No login details found in localStorage!", "warning");
         return;
     }
 
     if (adminDetails[0] === userName && adminDetails[1] === password) {
-        console.log("Nice"); // Login success
+
     } else {
-        console.log("Invalid username or password");
+        Swal.fire("Invalid username or password", "warning");
     }
 }
 
@@ -35,7 +37,7 @@ function placeOrder() {
     if (itemName === "" || qty === "" || customerName === "" || customerPhoneNumber === "" || price === "") {
         Swal.fire("Missing Fields", "Please fill all the fields", "warning");
         return;
-    } else { 
+    } else {
         let total = qty * price;
 
         Swal.fire({
@@ -57,6 +59,39 @@ function placeOrder() {
             width: "350px"
         });
 
+        let tempCusArray = JSON.parse(localStorage.getItem("customers")) || [];
+
+        for (let i = 0; i < tempCusArray.length; i++) {
+            if (tempCusArray[i].phone === customerPhoneNumber) {
+
+                tempCusArray[i].orders.push({
+                    name: itemName,
+                    itemQty: qty,
+                    unitPrice: price
+                });
+
+                localStorage.setItem("customers", JSON.stringify(tempCusArray));
+                document.getElementById("orderBurgerName").value = "";
+                document.getElementById("orderQty").value = "1";
+                document.getElementById("customerName").value = "";
+                document.getElementById("customerPhone").value = "";
+                document.getElementById("orderBurgerPrice").value = "";
+                return;
+            }
+        }
+
+        tempCusArray.push({
+            name: customerName,
+            phone: customerPhoneNumber,
+            orders: [{
+                name: itemName,
+                itemQty: qty,
+                unitPrice: price
+            }]
+        });
+
+        localStorage.setItem("customers", JSON.stringify(tempCusArray));
+
         document.getElementById("orderBurgerName").value = "";
         document.getElementById("orderQty").value = "1";
         document.getElementById("customerName").value = "";
@@ -65,5 +100,17 @@ function placeOrder() {
     }
 }
 
+function searchCustomer() {
 
+    let customerPhone = document.getElementById("customerPhone").value.trim();
 
+    let tempArray = JSON.parse(localStorage.getItem("customers")) || [];
+
+    for (let i = 0; i < tempArray.length; i++) {
+        if (tempArray[i].phone === customerPhone) {
+            document.getElementById("customerName").value = tempArray[i].name;
+            return;
+        }
+    }
+    document.getElementById("customerName").value = "";
+}
